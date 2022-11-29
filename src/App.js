8bd96fcea54e09b876620499444a59d35f2d72fc
saif-abualkhair <81400848+saif-abualkhair@users.noreply.github.com>
2022-11-29 10:32:28 +0300

    a form with form validation test waiting for implementation tonight

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './nav/nav'
+import Form from './form/form'
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Nav></Nav>
+        <Form></Form>
         <div className='test'>
           <span className='text-dark-violet'>
             content
